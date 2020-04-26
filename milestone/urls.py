1d85c969c25ba4ec83cf39c84e819e2cee7a66cc
EC2 Default User <ec2-user@ip-172-31-88-128.ec2.internal>
2020-04-26 12:14:39 +0000

    circular import error

--- milestone/urls.py
+++ milestone/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from accounts import urls as urls_accounts
+from django_blog import urls as urls_blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^accounts/', include(urls_accounts)),
+    url(r'^blog/', include(urls_blog)),
 ]
