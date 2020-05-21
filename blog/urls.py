7e110e50190e4ba969867da466eaf98c3431ac58
elizabeth24602 <elizabethperrey19@outlook.com>
2020-05-21 17:08:30 +0000

    updates made to project

--- blog/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import include, path
-from .views import blog
-
-urlpatterns = [
-    path('blog/', blog, name='blog'),
--- blog/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def blog(request):
-    """A view that displays the blog page"""
-    return render(request, "blog.html")
