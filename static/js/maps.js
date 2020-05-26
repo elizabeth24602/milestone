970e5c0b29347d2c27034ff97783b50cc4cbc197
elizabeth24602 <elizabethperrey19@outlook.com>
2020-05-26 17:07:20 +0000

    added lightbox to products.html

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,27 +1,22 @@
 function initMap() {
-            var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 3,
-                center: {
-                    lat: 55.3781,
-                    lng: 3.4360
-                }
-            });
-        var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
-
-            var locations = [{
-                lat: 52.0339,
-                lng: -2.4236
-            }];
-
-            var markers = locations.map(function(location, i) {
-                return new google.maps.Marker({
-                    position: location,
-                    label: labels[i % labels.length]
-                });
-            });
-
-             var MarkerCluster = new MarkerClusterer(map, markers,
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-      };
-        
-    
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 55.3781,
+            lng: 3.4360
+        }
+    });
+    let labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+    let locations = [{
+        lat: 52.0339,
+        lng: -2.4236
+    }];
+    let markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+    let markerCluster = new MarkerClusterer(map, markers,
+        {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+}
