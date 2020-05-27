1b90eb8a5118dbfa695fbf1765d0b71dc08a2f5a
elizabeth24602 <elizabethperrey19@outlook.com>
2020-05-27 12:32:37 +0000

    final commit

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,19 +1,14 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: {
-            lat: 55.3781,
-            lng: 3.4360
-        }
+        zoom: 10,
+        center: {lat: 52.0559, lng: -2.7175}
     });
     let labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
-    let locations = [{
-        lat: 52.0339,
-        lng: -2.4236
-    }];
+    let locations = [new google.maps.LatLng(52.0339, -2.4236)];
     let markers = locations.map(function (location, i) {
         return new google.maps.Marker({
             position: location,
+            map: map,
             label: labels[i % labels.length]
         });
     });
